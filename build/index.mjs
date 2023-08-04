b35a6442a92a1b4f3bbab93f740608226de918f8
nicholas-yong <40938341+nicholas-yong@users.noreply.github.com>
2023-08-04 12:40:37 +0800

    remove build folder

--- build/index.mjs
+++
@@ -1,3 +0,0 @@
-import { MarumoHelper } from "./helper/marumo-helper.mjs";
-var marumoHelper = new MarumoHelper();
-await marumoHelper.startPolling(5000);
--- build/main.mjs
+++
@@ -1,5 +0,0 @@
-import { BookingBot } from "./helper/bot-helper.mjs";
-// var marumoHelper = new MarumoHelper();
-// await marumoHelper.startPolling(5000);
-const botMan = new BookingBot();
-botMan.initialiseSite();
